7e3d61770b0c6493137ab39a3e7a56229a526e15
Theophile Vaudour <theophile.vaudour@u-pusd.fr>
2022-04-21 11:03:44 +0200

    participe.php change

--- api/modele/participe.php
+++ api/modele/participe.php
@@ -4,39 +4,35 @@
         public $idPartie;
         public $duree;
         public $score;
-        public $role;
         public $estMort;
-        public $longitude;
-        public $latitude;
+        public $longitudeJoueur;
+        public $latitudeJoueur;
 		
 		public function __construct($tab){
 				$this->idJoueur = $tab["idJoueur"];
 				$this->idPartie = $tab["idPartie"];
 				$this->duree = $tab["duree"];
 				$this->score = $tab["score"];
-				$this->role = $tab["role"];
 				$this->estMort = $tab["estMort"];
-				$this->longitude = $tab["longitude"];
-				$this->latitude = $tab["latitude"];
+				$this->longitudeJoueur = $tab["longitude"];
+				$this->latitudeJoueur = $tab["latitude"];
 		}
 		
 		public function getIdJoueur(){		return $this->idJoueur; 		}
 		public function getIdPartie(){		return $this->idPartie; 		}
 		public function getDuree(){		return $this->duree; 		}
 		public function getScore(){		return $this->score; 		}
-		public function getRole(){		return $this->role; 		}
 		public function getEstMort(){		return $this->estMort; 		}
-		public function getLongitude(){		return $this->longitude; 		}
-		public function getLatitude(){		return $this->latitude; 		}
+		public function getLongitude(){		return $this->longitudeJoueur; 		}
+		public function getLatitude(){		return $this->latitudeJoueur; 		}
 		
 		public function setIdJoueur($idJoueur){	 $this->idJoueur; 		}
 		public function setIdPartie($idPartie){	 $this->idPartie; 		}
 		public function setDuree($duree){	     $this->duree; 		}
 		public function setScore($score){		 $this->score; 		}
-		public function setRole($role){		 $this->role; 		}
 		public function setEstMort($estMort){	 $this->estMort; 		}
-		public function setLongitude($longitude){	 $this->longitude; 		}
-		public function setLatitude($latitude){	 $this->latitude; 		}
+		public function setLongitude($longitude){	 $this->longitudeJoueur; 		}
+		public function setLatitude($latitude){	 $this->latitudeJoueur; 		}
 
 	}
 ?>
